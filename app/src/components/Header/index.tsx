6079a182ef90c58c096bec637f108f2b707b7f4e
20yuteo <20yuteo@gmail.com>
2023-06-28 00:08:25 +0900

    Headerを作成

---
+++ app/src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { Box, Button, Container, Flex, Heading, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+const Header = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
+    return (
+        <Box px={4} bg={useColorModeValue('gray.100', 'gray.900')}>
+            <Container maxW="container.lg">
+                <Flex as="header" py="4" justifyContent="space-between" alignItems="center">
+                    <Heading as='h1' fontSize="2xl" cursor="pointer" color={useColorModeValue('black', 'white')}>
+                        うんちくエウレーカクイズ
+                    </Heading>
+                    <Button size='lg' onClick={toggleColorMode}>
+                        {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                    </Button>
+                </Flex>
+            </Container>
+        </Box >
+    )
+}
+
+export default Header;
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { RouterProvider } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react';
 import router from './components/Router.tsx'
+import Header from './components/Header/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <Header />
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>,
 )
