b2910b210e57a069690d4964f585104cbb93018e
20yuteo <20yuteo@gmail.com>
2023-07-06 21:30:25 +0900

    questionListのスタイルを調整

--- app/src/pages/QuestionList.tsx
+++ app/src/pages/QuestionList.tsx
@@ -1,10 +1,61 @@
-const QuestionList = () => {
+import { Card, Heading, VStack, Container, Flex, Link } from "@chakra-ui/react"
 
-    return (
-        <>
-            This is Question List Page.
-        </>
-    )
-}
+const QuestionList = () => (
+    <>
+        <VStack spacing='24px' maxW="735px" margin="auto" paddingTop="16px">
+            <Container maxW='md' color='white'>
+                <Link textDecoration="none">
+                    <Flex direction="row" maxW="500">
+                        <Card
+                            overflow='hidden'
+                            variant='outline'
+                            minW="100px"
+                            maxH="1000px"
+                            textAlign="center"
+                            fontSize="4xl"
+                        >
+                            🦕
+                        </Card>
+                        <Heading as='h6' size='md' textAlign="center" marginLeft="8px">The perfect latte</Heading>
+                    </Flex>
+                </Link>
+            </Container>
+            <Container maxW='md' color='white'>
+                <Link textDecoration="none">
+                    <Flex direction="row" maxW="500">
+                        <Card
+                            overflow='hidden'
+                            variant='outline'
+                            minW="100px"
+                            maxH="1000px"
+                            textAlign="center"
+                            fontSize="4xl"
+                        >
+                            🦕
+                        </Card>
+                        <Heading as='h6' size='md' textAlign="center" marginLeft="8px">The perfect latte</Heading>
+                    </Flex>
+                </Link>
+            </Container>
+            <Container maxW='md' color='white'>
+                <Link textDecoration="none">
+                    <Flex direction="row" maxW="500">
+                        <Card
+                            overflow='hidden'
+                            variant='outline'
+                            minW="100px"
+                            maxH="1000px"
+                            textAlign="center"
+                            fontSize="4xl"
+                        >
+                            🦕
+                        </Card>
+                        <Heading as='h6' size='md' textAlign="center" marginLeft="8px">The perfect latte</Heading>
+                    </Flex>
+                </Link>
+            </Container>
+        </VStack>
+    </>
+)
 
 export default QuestionList
